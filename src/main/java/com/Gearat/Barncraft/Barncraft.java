794c45bf6796ffdb42977e6cff70e87d0a1baeb7
Gearat <gnaarf@gmail.com>
2014-05-14 21:21:56 -0400

    new file created

--- src/main/java/com/Gearat/Barncraft/Barncraft.java
+++ src/main/java/com/Gearat/Barncraft/Barncraft.java
@@ -1,5 +1,13 @@
 package com.Gearat.Barncraft;
 
+import cpw.mods.fml.common.Mod;
+
+
+
+
+@Mod(modid = Barncraft.MODID, version = Barncraft.VERSION)
 public class Barncraft {
+	public static final String MODID = "Barncraft";
+    public static final String VERSION = "0.0.1";
 
 }
---
+++ src/main/java/com/Gearat/Barncraft/Block/BlockCoop.java
@@ -0,0 +1,21 @@
+package com.Gearat.Barncraft.Block;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockCoop extends BlockContainer{
+
+	protected BlockCoop(Material p_i45386_1_) {
+		super(p_i45386_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World var1, int var2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
