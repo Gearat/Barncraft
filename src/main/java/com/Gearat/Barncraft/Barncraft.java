58d7386e96418f32a4c481e0bdaee8b51fe842e7
Gearat <gnaarf@gmail.com>
2014-05-14 22:03:03 -0400

    Now with one item!

--- src/main/java/com/Gearat/Barncraft/Barncraft.java
+++ src/main/java/com/Gearat/Barncraft/Barncraft.java
@@ -1,11 +1,13 @@
 package com.Gearat.Barncraft;
 
 import com.Gearat.Barncraft.CreativeTab.CreativeTabsBarncraft;
+import com.Gearat.Barncraft.Item.ItemTamingStick;
 import com.Gearat.Barncraft.TileEntity.CoopTileEntity;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -22,6 +24,7 @@ public class Barncraft {
     public static final String VERSION = "0.0.1";
     public static CreativeTabs tabBarncraft = new CreativeTabsBarncraft("Barncraft");
     public static CoopTileEntity Coop;
+    public static Item TamingStick;
     
     @Instance(value = "BarncraftID")
     public static Barncraft instance;
@@ -35,7 +38,9 @@ public class Barncraft {
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
   //  BlockCoop = new BlockCoop(Material.wood);
-    
+    TamingStick = new ItemTamingStick().setUnlocalizedName("TamingStick").setTextureName("TamingStick");
+    	
+    GameRegistry.registerItem(TamingStick, "TamingStick");
     GameRegistry.registerTileEntity(CoopTileEntity.class, "ChickenCoopTileEntity");
     }
 }
---
+++ src/main/java/com/Gearat/Barncraft/Item/ItemTamingStick.java
@@ -0,0 +1,14 @@
+package com.Gearat.Barncraft.Item;
+
+import com.Gearat.Barncraft.Barncraft;
+
+import net.minecraft.item.Item;
+
+public class ItemTamingStick extends Item{
+
+	public ItemTamingStick()
+	{
+	this.setCreativeTab(Barncraft.tabBarncraft);
+	}
+	
+}
